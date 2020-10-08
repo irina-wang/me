eadcf5bb292f2b4021d5ed5fb3131672031d439c
leovenom <leonardt@oputlook.com>
2020-10-08 20:49:56 +0100

    added images

--- js.js
+++ js.js
@@ -1,5 +1,10 @@
 new fullpage('#fullPage', {
   autoScrolling: true,
   navigation: true,
-  anchors: ['section1','section2','section3']
+  anchors: ['section1','section2','section3'],
+  navigationTooltips: ['Leonardt', 'About', 'Torres', 'Torres', 'Zerbini', 'Barrão','Esteves', 'Maquette', 'Contact'],
+  showActiveTooltip: true,
+  scrollingSpeed: 1000,
+  controlArrows: false,
+  slidesNavigation: true,
 })
