0b2a3c295b40ca74595f36fc3f7b412d65620fab
Irina Wang <irina.wang@outlook.com>
2022-01-15 01:18:51 -0500

    change nav name

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ new fullpage('#fullPage', {
   autoScrolling: true,
   navigation: true,
   anchors: ['section1','section2','section3'],
-  navigationTooltips: ['Home', 'About', 'Torres', 'Others', 'Maquettes', 'Contact'],
+  navigationTooltips: ['Home', 'Current Project', 'Coding', 'Curriculum', 'Photography', 'Contact'],
   showActiveTooltip: true,
   scrollingSpeed: 1000,
   controlArrows: false,
