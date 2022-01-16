0bee9906a415aff628a765901e935a28eb62c548
Irina Wang <irina.wang@outlook.com>
2022-01-16 02:39:17 -0500

    update navigation

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ new fullpage('#fullPage', {
   autoScrolling: true,
   navigation: true,
   anchors: ['home', 'thesis', 'coding', 'photography', 'contact'],
-  navigationTooltips: ['Home', 'Thesis Project', 'Coding', 'Photography', 'Contact'],
+  navigationTooltips: ['Home', 'Curriculum Design', 'Coding Projects', 'Photography', 'Contact'],
   showActiveTooltip: true,
   scrollingSpeed: 1000,
   controlArrows: false,
